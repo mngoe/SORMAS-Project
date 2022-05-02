4e9fb9ed9def5be962128dcb296421fbfdbe20e1
maxime ngoe <maxime.ngoe@gmail.com>
2022-05-02 22:58:05 +0100

    ajustement Pathogen

--- sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/surveillance/components/disease/burden/PythogentBurdenComponent.java
+++ sormas-ui/src/main/java/de/symeda/sormas/ui/dashboard/surveillance/components/disease/burden/PythogentBurdenComponent.java
@@ -115,7 +115,8 @@ public class PythogentBurdenComponent extends VerticalLayout{
 		for(String ptt : newList){
 			if(ptt == newList.get(0)){
 				TextField textField = new TextField(ptt);
-				String text = I18nProperties.getCaption(PathogenTestType.ANTIBODY_DETECTION)+" : ";
+				Label label = new Label(I18nProperties.getCaption(PathogenTestType.ANTIBODY_DETECTION)+" : ");
+				addComponent(title);
 				textField.setValue(text+newOccurence.get(0));
 				textField.setWidth("55%");
 				textField.setReadOnly(true);
